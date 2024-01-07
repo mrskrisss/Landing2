bb6c930cbaa8f0512afb90511c4b3027e6a09f25
mrskrisss <150622099+mrskrisss@users.noreply.github.com>
2024-01-07 22:50:34 +0300

    added adaptive

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -575,9 +575,46 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"ebWYT":[function(require,module,exports) {
-$(document).ready(function() {
-    $(".slider").slick();
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    mousewheel: true,
+    keyboard: true
 });
+const burger = document.querySelector("#burger");
+const popup = document.querySelector("#popup");
+const header = document.header;
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const list = document.querySelector("#list").cloneNode(1);
+// При клике на иконку burger вызываем ф-ию burgerHandler
+burger.addEventListener("click", burgerHandler);
+// Выполняем действия при клике ..
+function burgerHandler(e) {
+    e.preventDefault();
+    // Переключаем стили элементов при клике
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+    header.classList.toggle("noscroll");
+    renderPopup();
+}
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+    popup.appendChild(list);
+}
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(list.children);
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link)=>{
+    link.addEventListener("click", closeOnClick);
+});
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+    popup.classList.remove("open");
+    burger.classList.remove("active");
+    header.classList.remove("noscroll");
+}
 
 },{}]},["b3anl","ebWYT"], "ebWYT", "parcelRequiredd31")
 
